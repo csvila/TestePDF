517d6ef62bd87b751b6528ab987a02509b9ab2a0
Cesar Vilarim <cesar.vilarim@contractor-mc1.com>
2020-12-03 22:30:28 -0300

    First commit

---
+++ TesteSpirePDF/Program.cs
@@ -0,0 +1,22 @@
+﻿using Spire.Pdf;
+using System;
+
+namespace TesteSpirePDF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 2)
+                throw new Exception("Necessário informar o arquivo de entrada e pasta de saída.");
+
+            //open an existing pdf document
+            PdfDocument doc = new PdfDocument(args[0]);
+            //Split one PDF document to multiple files
+            String pattern = $@"{args[1]}\SplitDocument-{0}.pdf";
+            doc.Split(pattern);
+            String lastPageFileName = String.Format(pattern, doc.Pages.Count - 1);
+            doc.Close();
+        }
+    }
+}
